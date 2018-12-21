9c95c90bebaf57de235438b69ed8759ddfb7d884
Dima <echelon1109@gmail.com>
2018-12-21 18:00:26 +0200

    added resize button, edited css file

--- app/components/main/Main.js
+++ app/components/main/Main.js
@@ -11,7 +11,7 @@ class Main extends Component {
             left: 0,
             top:0
         };
-        this.moveMain = this.moveMain.bind(this);
+        this.onMouseDown = this.onMouseDown.bind(this);
 
     }
     renderMain() {
@@ -23,36 +23,36 @@ class Main extends Component {
                 <NeuronList neuronListNum={neuronListNum} neuronColor={neuronColor} key={key} list = {list} />
             )
         });
-        return lists
-    }
-    moveMain(e) {
-        if(e.ctrlKey) {
-            e.preventDefault();
-            const coordX = e.clientX;
-            const coordY = e.clientY;
-            const posX = this.state.left;
-            const posY = this.state.top;
-            window.onmousemove = e => {
-                let left = e.clientX-coordX+posX;
-                let top = e.clientY-coordY+posY;
 
-                this.setState({
-                    left,
-                    top
-                });
-            };
-            window.onmouseup = () => window.onmousemove = null
-        }
+        return lists;
+    }
+
+    onMouseDown(e) {
+        e.preventDefault();
+
+        const coordX = e.clientX;
+        const coordY = e.clientY;
+        const posX = this.state.left;
+        const posY = this.state.top;
+
+        if (!e.ctrlKey) return;
+
+        window.onmousemove = e => {
+            let left = e.clientX - coordX + posX;
+            let top = e.clientY - coordY + posY;
+
+            this.setState({ left, top });
+        };
+
+        window.onmouseup = () => window.onmousemove = null;
     }
 
     render() {
-
         const style = {
-
             transform: `scale(${this.props.sliderValue/50}) translate(${this.state.left}px, ${this.state.top}px)`
         };
         return (
-            <div onMouseDown={this.moveMain} style={style} className="main">
+            <div onMouseDown={this.onMouseDown} style={style} className="main">
                 {this.renderMain()}
             </div>
         )
--- app/components/slider/Slider.js
+++ app/components/slider/Slider.js
@@ -1,15 +1,24 @@
 import React, {Component} from 'react'
 import { Slider } from 'material-ui-slider';
 import {connect} from 'react-redux'
-import actionChangeSize from '../../actions/actionChangeSize'
+import actionChangeSize from '../../actions/actionChangeSize';
+import Button from '@material-ui/core/Button';
+
 class Slide extends Component {
     render() {
-        return <Slider
-            min='20'
-            max='200'
-            onChange={value => this.props.setSliderValue(value)}
-            style={{width: '200px', marginLeft: '200px'}}
-        />
+        return (
+            <React.Fragment>
+                <Slider
+                    type='range'
+                    value={this.props.sliderValue}
+                    min='20'
+                    max='200'
+                    onChange={value => this.props.setSliderValue(value)}
+                    style={{width: '200px', marginLeft: '200px'}}
+                />
+                <Button className='refresh__btn' onClick={() => this.props.setSliderValue(50)}>Refresh</Button>
+            </React.Fragment>
+        )
     }
 }
 function mapStateToProps(state) {
