20576beff0e20edcbe59ba4b8076c36c678b5fe9
Dima <echelon1109@gmail.com>
2019-01-03 13:25:42 +0200

    added webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
  
 module.exports = {
-	devtool: 'source-map', 
-    entry: [
+	devtool: 'source-map',
+        entry: [
        path.resolve(__dirname, 'app/app.js'),
        path.resolve(__dirname, 'app/style/main.css')
-   ], 
+   ],
     output:{
         path: path.resolve(__dirname, './public'),     // путь к каталогу выходных файлов - папка public
         publicPath: '/public/',
@@ -13,17 +13,22 @@ module.exports = {
     },
 	resolve: {
 	    alias: {
-	        components: path.resolve(__dirname, 'app/components')
+	        components: path.resolve(__dirname, 'app/components'),
+	        settings: path.resolve(__dirname, 'app/components/settings/setting')
         }
     },
     module:{
-        rules:[
+
+        rules:[
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
                 loader: "babel-loader",
                 options:{
-                    presets:["@babel/preset-env", "@babel/preset-react"]
+                    presets:["@babel/preset-env", "@babel/preset-react"],
+                    plugins: [
+                        "@babel/plugin-proposal-class-properties"
+                    ]
                 }
             },
 			{
